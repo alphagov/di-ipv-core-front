702528e5b2e4ae414fa04263be7bdccd93b9f9ba
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2022-06-13 15:02:46 +0100

    WIP

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
-require("express");
+const express = require('express')
+const session = require('express-session')
+const AWS = require('aws-sdk')
+const DynamoDBStore = require('connect-dynamodb')(session)
+
 require("express-async-errors");
 
-const { PORT, SESSION_SECRET } = require("./lib/config");
+const app = express()
+
+AWS.config.update({
+  region: 'eu-west-2',
+  endpoint: 'http://localhost:8000'
+})
+const dynamodb = new AWS.DynamoDB()
+
+const { PORT } = require("./lib/config");
 const { setup } = require("hmpo-app");
 
 const loggerConfig = {
@@ -10,22 +22,43 @@ const loggerConfig = {
   app: false,
 };
 
-const sessionConfig = {
-  cookieName: "ipv_core_service_session",
-  secret: SESSION_SECRET,
-};
 
 const { router } = setup({
   config: { APP_ROOT: __dirname },
   port: PORT,
   logs: loggerConfig,
-  session: sessionConfig,
+  session: false,
+  redis: false,
   urls: {
     public: "/public",
   },
   publicDirs: ["../dist/public"],
   dev: true,
 });
+
+app.use(session({
+  secret: 'my-secret',
+  resave: false,
+  saveUninitialized: false,
+  store: new DynamoDBStore({
+    client: dynamodb,
+    table: 'session'
+  })
+}));
+
+app.use((req, res, next) => {
+  if (!req.session.views) {
+    req.session.views = 0
+  }
+  next()
+});
+
+app.get('/private', (req, res) => {
+  const numberOfViews = ++req.session.views
+  res.send(`The /private page has been visited ${ numberOfViews } times.`)
+});
+
+
 
 router.use("/oauth2", require("./app/oauth2/router"));
 router.use("/credential-issuer", require("./app/credential-issuer/router"));
